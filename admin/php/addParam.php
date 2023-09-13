8e3eb2f25baceca7c00efe5c4bfe393a59547fda
Nigel Powell <nigel@thesadsongco.com>
2023-09-13 19:01:04 +0100

    clash finder mobile initial

---
+++ admin/php/addParam.php
@@ -0,0 +1,31 @@
+<?php
+
+require_once("../../../secure/mlc/db_connect.php");
+
+
+foreach($_POST as $key=>$value) {
+    if ($key != "submit") {
+        $params[] = [$key, $value, $value];
+    }
+}
+
+// echo "<pre>"; print_r($params);echo "</pre>";
+foreach ($params as $param) {
+    try {
+        $query = "INSERT INTO Params VALUES (?, ?)
+            ON DUPLICATE KEY UPDATE param_value = ?;";
+        $stmt = $db->prepare($query);
+        $stmt->execute($param);
+    }
+    catch(PDOException $e) {
+        echo $e->getmessage();
+        exit;
+    }
+}
+
+
+echo "Festival Parameter Updated";
+
+require_once("../../../secure/mlc/db_disconnect.php");
+
+?>
--- api/getShowsMobile.php
+++ api/getShowsMobile.php
@@ -28,10 +28,16 @@ try {
                 ;";
     
     $shows = $db->query($query)->fetchAll(PDO::FETCH_ASSOC);
+    $query = "SELECT param_name, param_value FROM Params";
+    $params = $db->query($query)->fetchAll(PDO::FETCH_ASSOC);
 } catch (PDOException $e) {
     echo $e->getMessage();
 }
 
+$params_arr = [];
+foreach($params as $param) {
+    $params_arr[$param["param_name"]] = $param["param_value"];
+}
 
 include ("./includes/colourPalette.php");
 $colours = ["#09aa09", "#0959aa", "#aa09aa", "#aa5909"];
@@ -46,28 +52,40 @@ foreach ($venues_res as $venue) {
     $colours[] = $venue["colour"];
 }
 
-$festival_start_time = new DateTime("2023-08-30 16:00:00");
+$festival_start_time = new DateTime($params_arr["festivalStart"]);
+$festival_end_time = new DateTime($params_arr["festivalEnd"]);
 
-// $prev_show["end_time"] = $festival_start_time;
+$prev_show["end_time"] = $params_arr["festivalStart"];
 
 foreach ($shows as &$show) {
     $show["colour"] = array_shift($venues[$show["venue_id"]]["pallette"]);
     $venues[$show["venue_id"]]["pallette"][] = $show["colour"];
     $show_start = new DateTime($show["start_time"]);
-    $diff_in_seconds = $show_start->getTimestamp() - $festival_start_time->getTimestamp();
+    $prev_show_end = new DateTime($prev_show["end_time"]);
+    $diff_in_seconds = $show_start->getTimestamp() - $prev_show_end->getTimestamp();
     $show["pixel_position"] = floor($diff_in_seconds / 60) * 3;
     $prev_show = $show;
 }
 
 // echo "<pre>"; print_r($shows); echo "</pre>";
 
+$festival_start_hour = $festival_start_time->format('H');
+if ($festival_start_hour > 12) $festival_start_hour -= 12;
+
+$festival_end_hour = $festival_end_time->format('H');
+if ($festival_end_hour > 12) $festival_end_hour -= 12;
+
 $output["time_guide"] = [];
 
-for ($i = 4; $i < 12; $i++) {
+for ($i = $festival_start_hour; $i <= $festival_end_hour; $i++) {
+    $clock_twenty_four = $i + 12;
+    $time_since_start = new DateTime("2023-08-30 $clock_twenty_four:00:00");
+    $diff_in_seconds = $time_since_start->getTimestamp() - $festival_start_time->getTimestamp();
+    $timefactor = floor($diff_in_seconds / 60) * 3;
     if ($i == 4) {
-        $output["time_guide"][] = ["time"=>"$i pm", "open"=>true];
+        $output["time_guide"][] = ["time"=>"$i pm", "open"=>true, "timefactor"=>$timefactor];
     } else {
-        $output["time_guide"][] = ["time"=>"$i pm"];
+        $output["time_guide"][] = ["time"=>"$i pm", "timefactor"=>$timefactor];
     }
 }
 
